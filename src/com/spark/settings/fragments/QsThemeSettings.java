c413af4d9cae3186843efd69c1cc5be32fa001a2
spkal01 <kalligeross@gmail.com>
2021-06-30 21:14:36 +0530

    Fireworks:Introduce Brightness Slider Styles [2/2]

--- src/com/spark/settings/fragments/QsThemeSettings.java
+++ src/com/spark/settings/fragments/QsThemeSettings.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2020-2021 The Spark Project
+ * Copyright (C) 2021 Spark
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,51 +16,171 @@
 
 package com.spark.settings.fragments;
 
-import static android.os.UserHandle.USER_CURRENT;
 import static android.os.UserHandle.USER_SYSTEM;
 
-import android.content.BroadcastReceiver;
+import android.app.AlertDialog;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.DialogInterface;
+import android.content.ServiceConnection;
 import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.content.pm.PackageManager;
+import android.database.ContentObserver;
+import android.net.Uri;
+
+import android.os.SystemProperties;
 import android.os.Bundle;
-import android.provider.SearchIndexableResource;
+import android.os.Handler;
+import android.os.UserHandle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import androidx.preference.*;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.preference.*;
-import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.internal.util.spark.SparkUtils;
-import com.android.internal.util.spark.ThemesUtils;
-
-import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
-import com.android.settings.development.OverlayCategoryPreferenceController;
-import com.android.settings.display.FontPickerPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.Indexable;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
-import com.android.settingslib.search.SearchIndexable;
 
 import com.spark.settings.display.QsColorPreferenceController;
 import com.spark.settings.display.QsTileStylePreferenceController;
+import com.android.settings.R;
+import com.spark.settings.preferences.SystemSettingListPreference;
+import com.spark.settings.preferences.SystemSettingSwitchPreference;
+import com.android.internal.util.spark.SparkUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class QsThemeSettings extends DashboardFragment {
-    public static final String TAG = "QsThemeSettings";
+    private static final String TAG = "QsThemeSettings";
 
-    private Context mContext;
     private IOverlayManager mOverlayManager;
-    private IOverlayManager mOverlayService;
+    private PackageManager mPackageManager;
+    private static final String SLIDER_STYLE  = "slider_style";
+
+    private SystemSettingListPreference mSlider;
+    private Handler mHandler;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+                    getActivity().getActionBar().hide();
+        super.onCreate(savedInstanceState);
+        final ContentResolver resolver = getActivity().getContentResolver();
+        Context mContext = getContext();
+        mOverlayManager = IOverlayManager.Stub.asInterface(
+                ServiceManager.getService(Context.OVERLAY_SERVICE));
+        mSlider = (SystemSettingListPreference) findPreference(SLIDER_STYLE);
+        mCustomSettingsObserver.observe();
+    }
+
+    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+    private class CustomSettingsObserver extends ContentObserver {
+
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            Context mContext = getContext();
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SLIDER_STYLE  ),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(Settings.System.SLIDER_STYLE  ))) {
+                updateSlider();
+            }
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mSlider) {
+            mCustomSettingsObserver.observe();
+            return true;
+        }
+        return false;
+    }
+
+    private void updateSlider() {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        boolean sliderDefault = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 0;
+        boolean sliderOOS = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 1;
+        boolean sliderAosp = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 2;
+        boolean sliderRUI = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 3;
+
+        if (sliderDefault) {
+            setDefaultSlider(mOverlayManager);
+        } else if (sliderOOS) {
+            enableSlider(mOverlayManager, "com.android.theme.systemui_slider_oos");
+        } else if (sliderAosp) {
+            enableSlider(mOverlayManager, "com.android.theme.systemui_slider.aosp");
+        } else if (sliderRUI) {
+            enableSlider(mOverlayManager, "com.android.theme.systemui_slider.rui");
+        }
+    }
+
+    public static void setDefaultSlider(IOverlayManager overlayManager) {
+        for (int i = 0; i < SLIDERS.length; i++) {
+            String sliders = SLIDERS[i];
+            try {
+                overlayManager.setEnabled(sliders, false, USER_SYSTEM);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void enableSlider(IOverlayManager overlayManager, String overlayName) {
+        try {
+            for (int i = 0; i < SLIDERS.length; i++) {
+                String sliders = SLIDERS[i];
+                try {
+                    overlayManager.setEnabled(sliders, false, USER_SYSTEM);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+            overlayManager.setEnabled(overlayName, true, USER_SYSTEM);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void handleOverlays(String packagename, Boolean state, IOverlayManager mOverlayManager) {
+        try {
+            mOverlayManager.setEnabled(packagename,
+                    state, USER_SYSTEM);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final String[] SLIDERS = {
+        "com.android.theme.systemui_slider_oos",
+        "com.android.theme.systemui_slider.aosp",
+        "com.android.theme.systemui_slider.rui"
+    };
 
     @Override
     public int getMetricsCategory() {
@@ -72,22 +192,10 @@ public class QsThemeSettings extends DashboardFragment {
         return TAG;
     }
 
-
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.spark_settings_qsthemes;
     }
-
-
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
-                    getActivity().getActionBar().hide();
-        mOverlayService = IOverlayManager.Stub
-                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
-        mContext = getActivity();
-   }
 
     @Override
     protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
@@ -101,24 +209,4 @@ public class QsThemeSettings extends DashboardFragment {
         controllers.add(new QsTileStylePreferenceController(context));
         return controllers;
     }
-
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        final Context context = getActivity();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        final Context context = getActivity();
-    }
-
-    /**
-     * For Search.
-     */
-
-    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.spark_settings_qsthemes);
 }
