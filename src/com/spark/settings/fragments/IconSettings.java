15931962c0f6eaf28f1d86fdcbe93e88e7755725
spkal01 <kalligeross@gmail.com>
2021-07-06 20:28:25 +0530

    Fireworks: Nuke custom signal and wifi icons

--- src/com/spark/settings/fragments/IconSettings.java
+++ src/com/spark/settings/fragments/IconSettings.java
@@ -112,10 +112,6 @@ public class IconSettings extends DashboardFragment implements OnPreferenceChang
                 "android.theme.customization.adaptive_icon_shape"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.icon_pack"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.signal_icon"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.wifi_icon"));
         controllers.add(new SwitchStylePreferenceController(context));
         return controllers;
     }
