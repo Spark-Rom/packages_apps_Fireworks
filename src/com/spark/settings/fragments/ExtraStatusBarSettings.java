f71854946ae291afed6a7db360efbb19a55b5cce
spezi77 <spezi7713@gmx.net>
2021-08-05 16:58:57 +0530

    Fireworks: Improve Rounded corner settings

--- src/com/spark/settings/fragments/ExtraStatusBarSettings.java
+++ src/com/spark/settings/fragments/ExtraStatusBarSettings.java
@@ -97,9 +97,7 @@ public class ExtraStatusBarSettings extends SettingsPreferenceFragment implement
         // Rounded Corner Radius
         mCornerRadius = (CustomSeekBarPreference) findPreference(SYSUI_ROUNDED_SIZE);
         int resourceIdRadius = (int) ctx.getResources().getDimension(com.android.internal.R.dimen.rounded_corner_radius);
-        int cornerRadius = Settings.Secure.getIntForUser(ctx.getContentResolver(), Settings.Secure.SYSUI_ROUNDED_SIZE,
-                ((int) (resourceIdRadius / density)), UserHandle.USER_CURRENT);
-        mCornerRadius.setValue(cornerRadius);
+        mCornerRadius.setValue((int) (resourceIdRadius / density));
         mCornerRadius.setOnPreferenceChangeListener(this);
 
         // Rounded Content Padding
