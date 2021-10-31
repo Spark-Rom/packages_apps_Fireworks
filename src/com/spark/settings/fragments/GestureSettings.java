f665439db59453da22e9bd61cea8e6728247c021
spkal01 <kalligeross@gmail.com>
2021-10-31 15:55:58 +0100

    Move Tap To wake Preference to Fireworks [2/2]

--- src/com/spark/settings/fragments/GestureSettings.java
+++ src/com/spark/settings/fragments/GestureSettings.java
@@ -19,32 +19,47 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
-
+import com.android.settings.display.OverlayCategoryPreferenceController;
+import com.android.settings.display.TapToWakePreferenceController;
 import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
-
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import android.content.Context;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 import android.util.Log;
-
+import androidx.fragment.app.Fragment;
+import com.android.settings.dashboard.DashboardFragment;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
 
-public class GestureSettings extends SettingsPreferenceFragment implements
+public class GestureSettings extends DashboardFragment implements
         OnPreferenceChangeListener {
+
+    public static final String TAG = "GestureSettings";
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-        addPreferencesFromResource(R.xml.spark_settings_gestures);
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.spark_settings_gestures;
+    }
+
+    @Override
+    protected String getLogTag() {
+       return TAG;
     }
 
     @Override
@@ -58,4 +73,15 @@ public class GestureSettings extends SettingsPreferenceFragment implements
         return MetricsProto.MetricsEvent.SPARK_SETTINGS;
     }
 
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+   }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new TapToWakePreferenceController(context));
+        return controllers;
+    }
 }
