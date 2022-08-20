4d5a7d19d2e4fdcffc79b76ffdb41f0bcde2d5b0
spkal01 <kalligeross@gmail.com>
2022-08-20 11:48:12 +0300

    Fireworks: Prepare for A13

--- src/com/spark/settings/fragments/LockScreenSettings.java
+++ src/com/spark/settings/fragments/LockScreenSettings.java
@@ -28,8 +28,6 @@ import android.text.TextUtils;
 import android.view.View;
 import android.app.Activity;
 import android.net.Uri;
-import com.android.internal.util.spark.SparkUtils;
-import com.android.internal.util.spark.udfps.UdfpsUtils;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 import android.util.Log;
@@ -65,19 +63,6 @@ import static android.os.UserHandle.USER_CURRENT;
 public class LockScreenSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-    private static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
-    private static final String FINGERPRINT_ERROR_VIB = "fingerprint_error_vib";
-    private static final String UDFPS_CATEGORY = "udfps_category";
-    private static final String CUSTOM_CLOCK_FACE = Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_FACE;
-    private static final String DEFAULT_CLOCK = "com.android.keyguard.clock.DefaultClockController";
-
-    private ListPreference mLockClockStyles;
-    private FingerprintManager mFingerprintManager;
-    private SwitchPreference mFingerprintSuccessVib;
-    private SwitchPreference mFingerprintErrorVib;
-    private PreferenceCategory mUdfpsCategory;
-    private ListPreference mQuickPulldown;
-
     private Context mContext;
 
     @Override
@@ -88,43 +73,6 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
         final PackageManager mPm = getActivity().getPackageManager();
-        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
-        mFingerprintSuccessVib = (SwitchPreference) findPreference(FINGERPRINT_SUCCESS_VIB);
-        mFingerprintErrorVib = (SwitchPreference) findPreference(FINGERPRINT_ERROR_VIB);
-        if (mPm.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT) &&
-                 mFingerprintManager != null) {
-            if (!mFingerprintManager.isHardwareDetected()){
-                prefSet.removePreference(mFingerprintSuccessVib);
-                prefSet.removePreference(mFingerprintErrorVib);
-            } else {
-                mFingerprintSuccessVib.setChecked((Settings.System.getInt(getContentResolver(),
-                        Settings.System.FP_SUCCESS_VIBRATE, 1) == 1));
-                mFingerprintSuccessVib.setOnPreferenceChangeListener(this);
-                mFingerprintErrorVib.setChecked((Settings.System.getInt(getContentResolver(),
-                        Settings.System.FP_ERROR_VIBRATE, 1) == 1));
-                mFingerprintErrorVib.setOnPreferenceChangeListener(this);
-            }
-        } else {
-            prefSet.removePreference(mFingerprintSuccessVib);
-            prefSet.removePreference(mFingerprintErrorVib);
-        }
-
-        int qpmode = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0, UserHandle.USER_CURRENT);
-        mQuickPulldown = (ListPreference) findPreference("status_bar_quick_qs_pulldown");
-        mQuickPulldown.setValue(String.valueOf(qpmode));
-        mQuickPulldown.setSummary(mQuickPulldown.getEntry());
-        mQuickPulldown.setOnPreferenceChangeListener(this);
-
-        mUdfpsCategory = findPreference(UDFPS_CATEGORY);
-        if (!UdfpsUtils.hasUdfpsSupport(getContext())) {
-            prefSet.removePreference(mUdfpsCategory);
-        }
-        mLockClockStyles = (ListPreference) findPreference(CUSTOM_CLOCK_FACE);
-        String mLockClockStylesValue = getLockScreenCustomClockFace();
-        mLockClockStyles.setValue(mLockClockStylesValue);
-        mLockClockStyles.setSummary(mLockClockStyles.getEntry());
-        mLockClockStyles.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -135,57 +83,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mFingerprintSuccessVib) {
-            boolean value = (Boolean) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.FP_SUCCESS_VIBRATE, value ? 1 : 0);
-            return true;
-        } else if (preference == mFingerprintErrorVib) {
-            boolean value = (Boolean) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.FP_ERROR_VIBRATE, value ? 1 : 0);
-            return true;
-        } else if (preference == mQuickPulldown) {
-            int value = Integer.parseInt((String) newValue);
-            Settings.System.putIntForUser(resolver,
-                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, value,
-                    UserHandle.USER_CURRENT);
-            int index = mQuickPulldown.findIndexOfValue((String) newValue);
-            mQuickPulldown.setSummary(
-                    mQuickPulldown.getEntries()[index]);
-            return true;
-         } else if (preference == mLockClockStyles) {
-            setLockScreenCustomClockFace((String) newValue);
-            int index = mLockClockStyles.findIndexOfValue((String) newValue);
-            mLockClockStyles.setSummary(mLockClockStyles.getEntries()[index]);
-            return true;
-         }
         return false;
-    }
-
-    private String getLockScreenCustomClockFace() {
-        mContext = getActivity();
-        String value = Settings.Secure.getStringForUser(mContext.getContentResolver(),
-                CUSTOM_CLOCK_FACE, USER_CURRENT);
-
-        if (value == null || value.isEmpty()) value = DEFAULT_CLOCK;
-
-        try {
-            JSONObject json = new JSONObject(value);
-            return json.getString("clock");
-        } catch (JSONException ex) {
-        }
-        return value;
-    }
-
-    private void setLockScreenCustomClockFace(String value) {
-        try {
-            JSONObject json = new JSONObject();
-            json.put("clock", value);
-            Settings.Secure.putStringForUser(mContext.getContentResolver(), CUSTOM_CLOCK_FACE,
-                    json.toString(), USER_CURRENT);
-        } catch (JSONException ex) {
-        }
     }
 
     @Override
--- src/com/spark/settings/fragments/ThemeSettings.java
+++ src/com/spark/settings/fragments/ThemeSettings.java
@@ -5,7 +5,6 @@ import com.android.internal.logging.nano.MetricsProto;
 import android.os.Bundle;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import com.android.internal.util.spark.SparkUtils;
 import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
 import com.android.settings.development.OverlayCategoryPreferenceController;
@@ -64,27 +63,6 @@ public class ThemeSettings extends DashboardFragment implements
     public static final String TAG = "ThemeSettings";
     private Context mContext;
 
-    private IOverlayManager mOverlayManager;
-    private IOverlayManager mOverlayService;
-    private Handler mHandler;
-    private static final String QS_FOOTER_TEXT_STRING = "qs_footer_text_string";
-    private static final String ALT_SETTINGS_LAYOUT = "alt_settings_layout";
-    private static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
-    private static final String USE_STOCK_LAYOUT = "use_stock_layout";
-    private static final String DISABLE_USERCARD = "disable_usercard";
-    private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
-    private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
-    private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
-
-    private SystemSettingEditTextPreference mFooterString;
-    private SystemSettingListPreference mSettingsDashBoardStyle;
-    private ListPreference mTileAnimationStyle;
-    private ListPreference mTileAnimationDuration;
-    private SystemSettingSwitchPreference mAltSettingsLayout;
-    private ListPreference mTileAnimationInterpolator;
-    private SystemSettingSwitchPreference mUseStockLayout;
-    private SystemSettingSwitchPreference mDisableUserCard; 
-
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -94,47 +72,6 @@ public class ThemeSettings extends DashboardFragment implements
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
-        mFooterString = (SystemSettingEditTextPreference) findPreference(QS_FOOTER_TEXT_STRING);
-        mFooterString.setOnPreferenceChangeListener(this);
-        String footerString = Settings.System.getString(getContentResolver(),
-                QS_FOOTER_TEXT_STRING);
-        if (footerString != null && !footerString.isEmpty())
-            mFooterString.setText(footerString);
-        else {
-            mFooterString.setText("Spark");
-            Settings.System.putString(getActivity().getContentResolver(),
-                    Settings.System.QS_FOOTER_TEXT_STRING, "Spark");
-        }
-        mSettingsDashBoardStyle = (SystemSettingListPreference) findPreference(SETTINGS_DASHBOARD_STYLE);
-        mSettingsDashBoardStyle.setOnPreferenceChangeListener(this);
-        mAltSettingsLayout = (SystemSettingSwitchPreference) findPreference(ALT_SETTINGS_LAYOUT);
-        mAltSettingsLayout.setOnPreferenceChangeListener(this);
-        mUseStockLayout = (SystemSettingSwitchPreference) findPreference(USE_STOCK_LAYOUT);
-        mUseStockLayout.setOnPreferenceChangeListener(this);
-        mDisableUserCard = (SystemSettingSwitchPreference) findPreference(DISABLE_USERCARD);
-        mDisableUserCard.setOnPreferenceChangeListener(this);
-
-        mTileAnimationStyle = (ListPreference) findPreference(PREF_TILE_ANIM_STYLE);
-        int tileAnimationStyle = Settings.System.getIntForUser(resolver,
-                Settings.System.ANIM_TILE_STYLE, 0, UserHandle.USER_CURRENT);
-        mTileAnimationStyle.setValue(String.valueOf(tileAnimationStyle));
-        updateTileAnimationStyleSummary(tileAnimationStyle);
-        updateAnimTileStyle(tileAnimationStyle);
-        mTileAnimationStyle.setOnPreferenceChangeListener(this);
-
-        mTileAnimationDuration = (ListPreference) findPreference(PREF_TILE_ANIM_DURATION);
-        int tileAnimationDuration = Settings.System.getIntForUser(resolver,
-                Settings.System.ANIM_TILE_DURATION, 2000, UserHandle.USER_CURRENT);
-        mTileAnimationDuration.setValue(String.valueOf(tileAnimationDuration));
-        updateTileAnimationDurationSummary(tileAnimationDuration);
-        mTileAnimationDuration.setOnPreferenceChangeListener(this);
-
-        mTileAnimationInterpolator = (ListPreference) findPreference(PREF_TILE_ANIM_INTERPOLATOR);
-        int tileAnimationInterpolator = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.ANIM_TILE_INTERPOLATOR, 0, UserHandle.USER_CURRENT);
-        mTileAnimationInterpolator.setValue(String.valueOf(tileAnimationInterpolator));
-        updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
-        mTileAnimationInterpolator.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -145,81 +82,8 @@ public class ThemeSettings extends DashboardFragment implements
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mFooterString) {
-            String value = (String) newValue;
-            if (value != "" && !value.isEmpty())
-                Settings.System.putString(getActivity().getContentResolver(),
-                        Settings.System.QS_FOOTER_TEXT_STRING, value);
-            else {
-                mFooterString.setText("Spark");
-                Settings.System.putString(getActivity().getContentResolver(),
-                        Settings.System.QS_FOOTER_TEXT_STRING, "Spark");
-            }
-            return true;
-        } else if (preference == mSettingsDashBoardStyle) {
-            SparkUtils.showSettingsRestartDialog(getContext());
-            return true;
-        } else if (preference == mAltSettingsLayout) {
-            SparkUtils.showSettingsRestartDialog(getContext());
-            return true;
-        } else if (preference == mUseStockLayout) {
-            SparkUtils.showSettingsRestartDialog(getContext());
-            return true;
-        } else if (preference == mDisableUserCard) {
-            SparkUtils.showSettingsRestartDialog(getContext());
-            return true;
-        } else if (preference == mTileAnimationStyle) {
-            int tileAnimationStyle = Integer.valueOf((String) newValue);
-            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_STYLE,
-                    tileAnimationStyle, UserHandle.USER_CURRENT);
-            updateTileAnimationStyleSummary(tileAnimationStyle);
-            updateAnimTileStyle(tileAnimationStyle);
-            return true;
-        } else if (preference == mTileAnimationDuration) {
-            int tileAnimationDuration = Integer.valueOf((String) newValue);
-            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_DURATION,
-                    tileAnimationDuration, UserHandle.USER_CURRENT);
-            updateTileAnimationDurationSummary(tileAnimationDuration);
-            return true;
-        } else if (preference == mTileAnimationInterpolator) {
-            int tileAnimationInterpolator = Integer.valueOf((String) newValue);
-            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_INTERPOLATOR,
-                    tileAnimationInterpolator, UserHandle.USER_CURRENT);
-            updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
-            return true;
-        }
         return false;
     }
-
-	private void updateTileAnimationStyleSummary(int tileAnimationStyle) {
-        String prefix = (String) mTileAnimationStyle.getEntries()[mTileAnimationStyle.findIndexOfValue(String
-                .valueOf(tileAnimationStyle))];
-        mTileAnimationStyle.setSummary(getResources().getString(R.string.qs_set_animation_style, prefix));
-    }
-
-     private void updateTileAnimationDurationSummary(int tileAnimationDuration) {
-        String prefix = (String) mTileAnimationDuration.getEntries()[mTileAnimationDuration.findIndexOfValue(String
-                .valueOf(tileAnimationDuration))];
-        mTileAnimationDuration.setSummary(getResources().getString(R.string.qs_set_animation_duration, prefix));
-    }
-
-    private void updateTileAnimationInterpolatorSummary(int tileAnimationInterpolator) {
-        String prefix = (String) mTileAnimationInterpolator.getEntries()[mTileAnimationInterpolator.findIndexOfValue(String
-                .valueOf(tileAnimationInterpolator))];
-        mTileAnimationInterpolator.setSummary(getResources().getString(R.string.qs_set_animation_interpolator, prefix));
-    }
-
-    private void updateAnimTileStyle(int tileAnimationStyle) {
-        if (mTileAnimationDuration != null) {
-            if (tileAnimationStyle == 0) {
-                mTileAnimationDuration.setSelectable(false);
-                mTileAnimationInterpolator.setSelectable(false);
-            } else {
-                mTileAnimationDuration.setSelectable(true);
-                mTileAnimationInterpolator.setSelectable(true);
-            }
-        }
-        }
 
     @Override
     protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
