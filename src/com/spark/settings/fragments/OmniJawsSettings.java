e8f2536dab885134801b01022ac3b23a70906f65
minaripenguin <minaripenguin@users.noreply.github.com>
2022-12-26 13:34:04 +0800

    settings: Import Weather service API
    Co-authored-by: Trishiraj <gamerprince.exp@gmail.com>
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Co-authored-by: LorDClockaN <lordclockan@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/spark/settings/fragments/OmniJawsSettings.java
+++ src/com/spark/settings/fragments/OmniJawsSettings.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
+*/
 
 package com.spark.settings.fragments;
 
@@ -186,4 +187,3 @@ public class OmniJawsSettings extends SettingsPreferenceFragment implements
                 }
     };
 }
-*/
