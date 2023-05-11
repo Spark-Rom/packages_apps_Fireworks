03d770f43091d1b26a7f565bcbd61f8635a76cc1
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-11 18:03:28 +0300

    Fireworks: Settings customization

--- src/com/spark/settings/fragments/ThemeSettings.java
+++ src/com/spark/settings/fragments/ThemeSettings.java
@@ -8,6 +8,11 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
 import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemProperties;
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import androidx.preference.ListPreference;
@@ -22,6 +27,10 @@ import android.provider.Settings;
 import com.android.settings.R;
 import lineageos.providers.LineageSettings;
 import com.spark.settings.preferences.CustomSeekBarPreference;
+
+import com.spark.settings.preferences.SystemSettingListPreference;
+import com.spark.settings.preferences.SystemSettingSwitchPreference;
+import com.spark.settings.preferences.SystemSettingEditTextPreference;
 import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
@@ -31,6 +40,8 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settings.Utils;
 import android.util.Log;
+import com.android.internal.util.spark.ThemeUtils;
+import com.android.internal.util.spark.SparkUtils;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -47,6 +58,28 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
     private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
     private static final String HEADS_UP_TIMEOUT_PREF = "heads_up_timeout";
     private static final String KEY_VOLUME_PANEL_LEFT = "volume_panel_on_left";
+
+    private static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
+    private static final String SETTINGS_HEADER_IMAGE = "settings_header_image";
+    private static final String SETTINGS_HEADER_IMAGE_RANDOM = "settings_header_image_random";
+    private static final String SETTINGS_HEADER_TEXT = "settings_header_text";
+    private static final String SETTINGS_HEADER_TEXT_ENABLED = "settings_header_text_enabled";
+    private static final String SETTINGS_CONTEXTUAL_MESSAGES = "settings_contextual_messages";
+    private static final String USE_STOCK_LAYOUT = "use_stock_layout";
+    private static final String ABOUT_PHONE_STYLE = "about_card_style";
+    private static final String HIDE_USER_CARD = "hide_user_card";
+    
+    private ThemeUtils mThemeUtils;
+    private Handler mHandler;
+    private SystemSettingListPreference mSettingsDashBoardStyle;
+    private SystemSettingListPreference mAboutPhoneStyle;
+    private SystemSettingSwitchPreference mUseStockLayout;
+    private SystemSettingSwitchPreference mHideUserCard;
+    private Preference mSettingsHeaderImage;
+    private Preference mSettingsHeaderImageRandom;
+    private Preference mSettingsMessage;
+    private SystemSettingEditTextPreference mSettingsHeaderText;
+    private SystemSettingSwitchPreference mSettingsHeaderTextEnabled;
 
     private SwitchPreference mVolumePanelLeft;
     private CustomSeekBarPreference mHeadsUpTimeOut;
@@ -92,6 +125,27 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
         mVolumePanelLeft = (SwitchPreference) prefScreen.findPreference(KEY_VOLUME_PANEL_LEFT);
         mVolumePanelLeft.setChecked(isAudioPanelOnLeft);
 
+        mThemeUtils = new ThemeUtils(getActivity());
+        mCustomSettingsObserver.observe();
+
+        mSettingsDashBoardStyle = (SystemSettingListPreference) findPreference(SETTINGS_DASHBOARD_STYLE);
+        mSettingsDashBoardStyle.setOnPreferenceChangeListener(this);
+        mSettingsHeaderImageRandom = findPreference(SETTINGS_HEADER_IMAGE_RANDOM);
+        mSettingsHeaderImageRandom.setOnPreferenceChangeListener(this);
+        mSettingsMessage = findPreference(SETTINGS_CONTEXTUAL_MESSAGES);
+        mSettingsMessage.setOnPreferenceChangeListener(this);
+        mSettingsHeaderImage = findPreference(SETTINGS_HEADER_IMAGE);
+        mSettingsHeaderImage.setOnPreferenceChangeListener(this);
+        mUseStockLayout = (SystemSettingSwitchPreference) findPreference(USE_STOCK_LAYOUT);
+        mUseStockLayout.setOnPreferenceChangeListener(this);
+        mAboutPhoneStyle = (SystemSettingListPreference) findPreference(ABOUT_PHONE_STYLE);
+        mAboutPhoneStyle.setOnPreferenceChangeListener(this);
+        mHideUserCard = (SystemSettingSwitchPreference) findPreference(HIDE_USER_CARD);
+        mHideUserCard.setOnPreferenceChangeListener(this);
+        mSettingsHeaderText = (SystemSettingEditTextPreference) findPreference(SETTINGS_HEADER_TEXT);
+        mSettingsHeaderText.setOnPreferenceChangeListener(this);
+        mSettingsHeaderTextEnabled = (SystemSettingSwitchPreference) findPreference(SETTINGS_HEADER_TEXT_ENABLED);
+        mSettingsHeaderTextEnabled.setOnPreferenceChangeListener(this);
     }
 
     private static boolean isAudioPanelOnLeftSide(Context context) {
@@ -114,8 +168,99 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
             if (mShowAutoBrightness != null)
                 mShowAutoBrightness.setEnabled(value > 0);
             return true;
+        } else if (preference == mSettingsDashBoardStyle) {
+            mCustomSettingsObserver.observe();
+            return true;
+        } else if (preference == mUseStockLayout) {
+            SparkUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mHideUserCard) {
+            SparkUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mAboutPhoneStyle) {
+            SparkUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mSettingsHeaderImage) {
+            SparkUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mSettingsHeaderImageRandom) {
+            SparkUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mSettingsMessage) {
+            SparkUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mSettingsHeaderTextEnabled) {
+            boolean enable = (Boolean) newValue;
+            SystemProperties.set("persist.sys.settings.header_text_enabled", enable ? "true" : "false");
+            SparkUtils.showSettingsRestartDialog(getContext());
+            return true;
+        } else if (preference == mSettingsHeaderText) {
+            String value = (String) newValue;
+            SystemProperties.set("persist.sys.settings.header_text", value);
+            SparkUtils.showSettingsRestartDialog(getContext());
+            return true;
          }
          return false;
+    }
+
+
+    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+    private class CustomSettingsObserver extends ContentObserver {
+
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            Context mContext = getContext();
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SETTINGS_DASHBOARD_STYLE),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(Settings.System.SETTINGS_DASHBOARD_STYLE))) {
+                updateSettingsStyle();
+            }
+        }
+    }
+
+    private void updateSettingsStyle() {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        int settingsPanelStyle = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SETTINGS_DASHBOARD_STYLE, 0, UserHandle.USER_CURRENT);
+
+	// reset all overlays before applying
+        mThemeUtils.setOverlayEnabled("android.theme.customization.icon_pack.settings", "com.android.settings", "com.android.settings");
+
+	if (settingsPanelStyle == 0) return;
+
+        switch (settingsPanelStyle) {
+            case 1:
+              setSettingsStyle("com.android.system.settings.rui");
+              break;
+            case 2:
+              setSettingsStyle("com.android.system.settings.arc");
+              break;
+            case 3:
+              setSettingsStyle("com.android.system.settings.aosp");
+              break;
+            case 4:
+              setSettingsStyle("com.android.system.settings.mt");
+              break;
+            case 5:
+              setSettingsStyle("com.android.system.settings.card");
+              break;
+            default:
+              break;
+        }
+    }
+
+    public void setSettingsStyle(String overlayName) {
+       mThemeUtils.setOverlayEnabled("android.theme.customization.icon_pack.settings", overlayName, "com.android.settings");
     }
 
     private static int getDefaultDecay(Context context) {
